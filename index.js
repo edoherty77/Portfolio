7ce1958804cdbbeaead2bfdb6e569a638d2e90fe
Evan Doherty <edoherty77@gmail.com>
2020-09-09 02:02:50 -0400

    added sections

---
+++ index.js
@@ -0,0 +1,15 @@
+// Toggle mobile menu
+const toggle = document.querySelector('.toggle')
+const menu = document.querySelector('.menu')
+console.log(menu)
+
+function toggleMenu(){
+  if(menu.classList.contains("active")){
+    menu.classList.remove("active")
+    toggle.innerHTML = "<i class='fas fa-bars'></i>"
+  } else {
+    menu.classList.add('active');
+    toggle.innerHTML = "<i class='fa fa-times'></i>"
+  }
+}
+
