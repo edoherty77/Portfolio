ebbd1082154e5c7fc7620a82c3b0a7fe643c4d95
Evan Doherty <edoherty77@gmail.com>
2020-09-27 04:34:17 -0400

    navbar

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ function toggleMenu() {
 }
 
 const faders = document.querySelectorAll('.fade-in')
+const sliders = document.querySelectorAll('.slide-in')
 
 const appearOptions = {
-  threshold: 0.3,
-  rootMargin: '0px 0px -50px 0px',
+  threshold: 0,
+  rootMargin: '0px 0px -80px 0px',
 }
 
 const appearOnScroll = new IntersectionObserver(function (
@@ -37,4 +38,50 @@ appearOptions)
 
 faders.forEach((fader) => {
   appearOnScroll.observe(fader)
+})
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider)
+})
+
+const home = document.querySelector('#home')
+const portfolio = document.querySelector('#portfolio')
+
+const options = {
+  rootMargin: '-150px',
+}
+
+const test = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry)
+  })
+}, options)
+
+test.observe(home)
+
+$(document).ready(function () {
+  let scrollLink = $('.scroll')
+
+  //smooth scrolling
+  scrollLink.click(function (e) {
+    e.preventDefault()
+    $('body,html').animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+      },
+      1000,
+    )
+  })
+
+  //Active link switching
+  $(window).scroll(function () {
+    let scrollbarLocation = $(this).scrollTop()
+    scrollLink.each(function () {
+      let sectionOffset = $(this.hash).offset().top - 20
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).parent().addClass('active')
+        $(this).parent().siblings().removeClass('active')
+      }
+    })
+  })
 })
