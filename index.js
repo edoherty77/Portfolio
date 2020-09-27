fb50994f343e4c797efeefebf43b837226035319
Evan Doherty <edoherty77@gmail.com>
2020-09-27 02:19:43 -0400

    added fade effect

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ function toggleMenu() {
     toggle.innerHTML = "<i class='fa fa-times'></i>"
   }
 }
+
+const faders = document.querySelectorAll('.fade-in')
+
+const appearOptions = {
+  threshold: 0.3,
+  rootMargin: '0px 0px -50px 0px',
+}
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll,
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add('appear')
+      appearOnScroll.unobserve(entry.target)
+    }
+  })
+},
+appearOptions)
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader)
+})
