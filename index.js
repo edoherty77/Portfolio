a21f97b0e95d42513791c91da613121035596525
Evan Doherty <edoherty77@gmail.com>
2020-10-16 16:45:31 -0400

    padding on navlinks

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const toggle = document.querySelector('.toggle')
 const menu = document.querySelector('.menu')
 console.log(menu)
 
-// function toggleMenu() {
-//   if (menu.classList.contains('active')) {
-//     menu.classList.remove('active')
-//     toggle.innerHTML = "<i class='fas fa-bars'></i>"
-//   } else {
-//     menu.classList.add('active')
-//     toggle.innerHTML = "<i class='fa fa-times'></i>"
-//   }
-// }
-
 const navSlide = () => {
   const burger = document.querySelector('.burger')
   const nav = document.querySelector('.nav-links')
